a1e29afe2b2d2e2a2c85c717a9df63b82a7130d2
ZachoH <s203682@student.dtu.dk>
2024-07-26 11:03:47 +0200

    d

--- streamlit_tester_file.py
+++ streamlit_tester_file.py
@@ -1,1 +1,10 @@
-import streamlit
+import streamlit as st
+
+# Initialize connection.
+conn = st.connection('mariadb', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * from connect.location;', ttl=600)
+
+# Print results.
+print(df)
